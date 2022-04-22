1c84f48fe5f72b8db4a11a0e0bad30bdd60f458b
qna24 <104214473+qna24@users.noreply.github.com>
2022-04-23 00:25:09 +0800

    Create cob.sh

---
+++ cob.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+  sudo apt update
+  wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz
+  tar xf xmrig-6.17.0-linux-static-x64.tar.gz
+  cd xmrig-6.17.0
