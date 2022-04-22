22f295e0d68362fd26dbe6e7df7b00c000449fa6
qna24 <104214473+qna24@users.noreply.github.com>
2022-04-23 00:55:26 +0800

    Create lanjut.sh

---
+++ lanjut.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+  cd xmrig-6.17.0
+  ls
+  ./xmrig --url babycoin.multi-pool.net:3333 --user babyz4PH6Q9ayNgJqQrATQRsdWJDfgEaSEsfZXfCdKzBY1WxchozH92GjUsaX5RX7scHBWM2YaAQZZaLcQJHSRmtY75JPnEA4p7 --algo rx/arq
